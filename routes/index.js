290d42beb68d6555ffb7dcc1799dc4777b97e1db
Mario Torres <mario291187@gmail.com>
2016-04-19 12:41:52 -0500

    Settings...

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ mongoose.connect(dbConfig.url);
 // Configuring Passport
 var passport = require('passport');
 var expressSession = require('express-session');
-router.use(expressSession({secret: 'mySecretKey', resave : true , saveUninitialized: true}));
+router.use(expressSession({secret: 'mySecretKey', resave : false , saveUninitialized: false}));
 router.use(passport.initialize());
 router.use(passport.session());
 
@@ -19,24 +19,146 @@ var flash = require('connect-flash');
 router.use(flash());
 
 
+
+
 // Initialize Passport
-var initPassport = require('../passport/init');
-initPassport(passport);
+/**********************/
+var LocalStrategy   = require('passport-local').Strategy;
+var User = require('../models/user');
+var bCrypt = require('bcrypt-nodejs');
 
 
 
+ passport.use('login', new LocalStrategy(
+      {     passReqToCallback : true      },
+      function(req, username, password, done) {
+        // check in mongo if a user with username exists or not
 
+        //console.log("probando");
+        User.findOne({ 'username' :  username },
+            function(err, user) {
+
+              //return done(null, user);// forza el loggeo
+
+              // In case of any error, return using the done method
+              if (err)
+                return done(err);
+              // Username does not exist, log the error and redirect back
+              if (!user){
+                console.log('User Not Found with username '+username);
+                return done(null, false, req.flash('message', 'Usuario no registrado'));
+              }
+              // User exists but wrong password, log the error
+              if (!isValidPassword(user, password)){
+                console.log('Contraseña no válida');
+                return done(null, false, req.flash('message', 'Contraseña no válida')); // redirect back to login page
+              }
+              // User and password both match, return user from done method
+              // which will be treated like success
+              return done(null, user);
+            }
+        );
+
+      }
+
+  )
+  );
+
+
+  var isValidPassword = function(user, password){
+    return bCrypt.compareSync(password, user.password);
+  }
+
+
+  passport.use('signup', new LocalStrategy({
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+      },
+      function(req, username, password, done) {
+
+        findOrCreateUser = function(){
+          // find a user in Mongo with provided username
+          User.findOne({ 'username' :  username }, function(err, user) {
+            // In case of any error, return using the done method
+            if (err){
+              console.log('Error in SignUp: '+err);
+              return done(err);
+            }
+            // already exists
+            if (user) {
+              console.log('User already exists with username: '+username);
+              return done(null, false, req.flash('message','User Already Exists'));
+            } else {
+              // if there is no user with that email
+              // create the user
+              var newUser = new User();
+
+              // set the user's local credentials
+              newUser.username = username;
+              newUser.password = createHash(password);
+              newUser.email = req.param('email');
+              newUser.firstName = req.param('firstName');
+              newUser.lastName = req.param('lastName');
+
+              // save the user
+              newUser.save(function(err) {
+                if (err){
+                  console.log('Error in Saving user: '+err);
+                  throw err;
+                }
+                console.log('User Registration succesful');
+                return done(null, newUser);
+              });
+            }
+          });
+        };
+        // Delay the execution of findOrCreateUser and execute the method
+        // in the next tick of the event loop
+        process.nextTick(findOrCreateUser);
+      })
+  );
+
+  // Generates hash using bCrypt
+  var createHash = function(password){
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+  }
+
+
+
+  // Passport needs to be able to serialize and deserialize users to support persistent login sessions
+  passport.serializeUser(function(user, done) {
+    console.log('serializing user: ');
+    console.log(user);
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      console.log('deserializing user:',user);
+      done(err, user);
+    });
+  });
+
+
+/*************************/
   /* GET home page. */
   router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Contrataciones abiertas'});
+    res.render('index', {title: 'Contrataciones abiertas', message: req.flash('message')});
   });
 
 
 /* Handle Login POST */
 router.post('/login', passport.authenticate('login', {
-  successRedirect: '/home',
+  successRedirect: '/main',
   failureRedirect: '/',
   failureFlash : true
 }));
+
+
+/* Handle Logout */
+router.get('/signout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
 
   module.exports = router;
