0c8cfd34d4cffaa0045be51c80965aa75f5e028e
Mario Torres <mario291187@gmail.com>
2016-04-19 16:26:57 -0500

    UI

---
+++ public/javascript/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mtorres on 19/04/16.
+ */
+
+var nuevoProceso = $('#nuevo');
+
+nuevoProceso.click(function(){
+    if ( confirm("¿Está seguro de crear un nuevo proceso de contratación?") == true ){
+      
+    }
+});
+
