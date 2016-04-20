67257f7a524b8a80ff71d638d4fa05f8f119c6cc
Mario Torres <mario291187@gmail.com>
2016-04-19 21:55:18 -0500

    UI

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,6 +2,14 @@
  * Created by mtorres on 19/04/16.
  */
 
+
+/* Date picker */
+$(function () {
+    $('#date1, #date2, #date3, #date4, #date5, #date6, #date7').datetimepicker();
+
+});
+
+
 var nuevoProceso = $('#nuevo');
 
 nuevoProceso.click(function(){
