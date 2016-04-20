eb65c4d0fe2c47806841e7918170093aff15aa52
lromang <luis.roangarci@gmail.com>
2016-04-20 13:45:33 -0500

    hash stdin

--- hash.js
+++ hash.js
@@ -5,9 +5,8 @@ var hash = "";
 //console.log(process.argv);
 
 if ( typeof process.argv[2] != 'undefined'){
-    hash = bcrypt.hashSync("bacon", bcrypt.genSaltSync(10), null)
-    console.log (hash);
+  hash = bcrypt.hashSync(process.argv[2], bcrypt.genSaltSync(10), null);
+  console.log(hash);
 }else{
-
-console.log("proporciona un argumento");
+  console.log("proporciona un argumento");
 }
