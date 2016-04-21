ff1a90d528fad481409a9dbe794acbed3f8c2ac7
Mario Torres <mario291187@gmail.com>
2016-04-20 22:44:14 -0500

    API

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -14,7 +14,9 @@ var nuevoProceso = $('#nuevo');
 
 nuevoProceso.click(function(){
     if ( confirm("¿Está seguro de crear un nuevo proceso de contratación?") == true ){
-      
+      $.get("/nuevo_proceso",function(data){
+          $("#ocid").val(data.id);
+      });
     }
 });
 
