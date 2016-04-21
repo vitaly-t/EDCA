6dc47be8925f63a25d12c9087b6c8296d643f87c
Mario Torres <mario291187@gmail.com>
2016-04-20 23:18:08 -0500

    UI

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -6,7 +6,7 @@
 /* Date picker */
 $(function () {
     $('#lici_date1, #lici_date2, #lici_date3, #lici_date4, #lici_date5, #lici_date6, #lici_date7').datetimepicker();
-    $('#adju_date1, #adju_date2, #adju_date3').datetimepicker();
+    $('#adju_date1, #adju_date2, #adju_date3, #adju_date4').datetimepicker();
     $('#cont_date1, #cont_date2, #cont_date3, #cont_date4').datetimepicker();
 
 });
