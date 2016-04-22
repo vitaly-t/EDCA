8672607cb5fe617ad621ae51219a1dfe74e056d7
Mario Torres <mario291187@gmail.com>
2016-04-22 13:10:43 -0500

    Tender

--- sql/edca.sql
+++ sql/edca.sql
@@ -8,7 +8,7 @@ create table ContractingProcess (
 
 
 /* ORGANIZATION TYPE */
-CREATE TABLE organizationtype (
+CREATE TABLE OrganizationType (
     id integer NOT NULL,
     name character varying(50)
 );
@@ -25,14 +25,6 @@ create table Planning (
 	ContractingProcess_id int references ContractingProcess(id)
 	);
 
-create table budget (
-	id serial primary key, 
-	contractingprocess_id int references contractingprocess(id), 
-	planning_id int references planning(id), budget_source text, 
-	budget_description text, budget_amount decimal, 
-	budget_currency text, budget_project text, 
-	budget_projectID text, budget_uri text, rationale text 
-	);
 
 create table PlanningDocument (
 	id serial primary key, 
@@ -43,6 +35,15 @@ create table PlanningDocument (
 	date_published timestamp, 
 	date_modified timestamp, 
 	format text, language text 
+	);
+
+create table budget (
+	id serial primary key, 
+	contractingprocess_id int references contractingprocess(id), 
+	planning_id int references planning(id), budget_source text, 
+	budget_description text, budget_amount decimal, 
+	budget_currency text, budget_project text, 
+	budget_projectID text, budget_uri text, rationale text 
 	);
 
 create table BudgetDocument (
@@ -58,13 +59,45 @@ create table BudgetDocument (
 	);
 
 
-/* BUYER */
-create table buyer (
-	id serial primary key, 
-	contractingprocess_id int references ContractingProcess(id),
+/* TENDER */
+create table Tender(
+	id serial primary key,
+	ContractingProcess_id int references ContractingProcess(id),
+	title text, 
+	description text,
+	status text,
+	minvalue_amount decimal, 
+	minvalue_currency text,
+	value_amount decimal,
+	value_currency text, 
+	procurementmethod text,
+	procurementmethod_rationale text,
+	awardcriteria text, 
+	awardcriteria_details text,
+	submissionmethod text,
+	submissionmethod_details text, 
+	tenderperiod_startdate timestamp,
+	tenderperiod_enddate timestamp,
+	enquiryperiod_startdate timestamp, 
+	enquiryperiod_enddate timestamp,
+	hasenquiries int, 
+	eligibilitycriteria text, 
+	awardperiod_startdate timestamp,
+	awardperiod_enddate timestamp,
+	numberoftenderers int, 
+	amendment_date timestamp,
+	amendment_rationale text
+	);
+
+/* Tenderer (Organization) */
+create table Tenderer(
+	id serial primary key,
+	ContractingProcess_id int references ContractingProcess(id),
+	Tender_id int references Tender(id),
+	name text,
 	identifier_scheme text, 
 	identifier_legalname text, 
-	identifier_uri text, name text, 
+	identifier_uri text,  
 	address_streetaddres text, 
 	address_locality text, 
 	address_region text, 
@@ -76,3 +109,45 @@ create table buyer (
 	contactpoint_faxnumber text, 
 	contactpoint_url text
 	);
+
+/* ProcuringEntity (organization) */
+create table ProcuringEntity(
+	id serial primary key, 
+	ContractingProcess_id int references ContractingProcess(id),
+	Tender_id int references Tender(id),
+	name text,
+	identifier_scheme text, 
+	identifier_legalname text, 
+	identifier_uri text,  
+	address_streetaddres text, 
+	address_locality text, 
+	address_region text, 
+	address_postalcode text, 
+	address_contryname text,
+	contactpoint_name text, 
+	contactpoint_email text, 
+	contactpoint_telephone text, 
+	contactpoint_faxnumber text, 
+	contactpoint_url text
+	);
+
+/* BUYER (organization) */
+create table buyer (
+	id serial primary key, 
+	contractingprocess_id int references ContractingProcess(id),
+	name text,
+	identifier_scheme text, 
+	identifier_legalname text, 
+	identifier_uri text,  
+	address_streetaddres text, 
+	address_locality text, 
+	address_region text, 
+	address_postalcode text, 
+	address_contryname text,
+	contactpoint_name text, 
+	contactpoint_email text, 
+	contactpoint_telephone text, 
+	contactpoint_faxnumber text, 
+	contactpoint_url text
+	);
+
