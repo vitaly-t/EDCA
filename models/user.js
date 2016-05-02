414538c2164fb14d905a62b91eb94fb9caa53692
Mario Torres <mario291187@gmail.com>
2016-05-02 13:31:01 -0500

    ...

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User',{
+    fullname: String,
     username: String,
     password: String,
     email: String,
-    gender: String,
     address: String
 });
