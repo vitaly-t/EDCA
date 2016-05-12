d5f982bb482902901273faf121f445af0fcc707a
Mario Torres <mario291187@gmail.com>
2016-05-12 11:58:20 -0500

    Organizations

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -135,7 +135,27 @@ $("#contract_form").submit(function(event){
     event.preventDefault();
 });
 
+$('#myModalNewOrg').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data('org'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    //modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('#org-type').val(recipient);
+    //modal.find('.modal-body input').val(recipient)
+});
 
+$('#myModalEditOrg').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data('org'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    //modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('#org-type').val(recipient);
+    //modal.find('.modal-body input').val(recipient)
+});
 /* */
 
 $(function () {
