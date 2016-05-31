017f1b7f6671966e738d93fda5d05dc5bf16e456
Mario Torres <mario291187@gmail.com>
2016-05-31 12:29:58 -0500

    Release...

--- routes/index.js
+++ routes/index.js
@@ -840,34 +840,34 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
                 return ( x != null && x != '' && typeof x != "undefined");
             }
 
-            function getOrganizations(orgarray){
+            function getOrganizations(array){
                 var organizations = [];
-                for ( var i=0; i < orgarray.length; i++){
+                for ( var i=0; i < array.length; i++){
                     var organization={};
 
                     organization.identifier = {};
-                    if( checkValue(orgarray[i].identifier_scheme)  ){organization.identifier.scheme = orgarray[i].identifier_scheme;}
-                    if( checkValue(orgarray[i].identifier_id) ){organization.identifier.id = orgarray[i].identifier_id;}
-                    if( checkValue(orgarray[i].identifier_legalname) ){organization.identifier.legalName = orgarray[i].identifier_legalname;}
-                    if( checkValue(orgarray[i].identifier_uri) ){organization.identifier.uri = orgarray[i].identifier_uri;}
+                    if( checkValue(array[i].identifier_scheme)  ){organization.identifier.scheme = array[i].identifier_scheme;}
+                    if( checkValue(array[i].identifier_id) ){organization.identifier.id = array[i].identifier_id;}
+                    if( checkValue(array[i].identifier_legalname) ){organization.identifier.legalName = array[i].identifier_legalname;}
+                    if( checkValue(array[i].identifier_uri) ){organization.identifier.uri = array[i].identifier_uri;}
 
                     //additionalIdentifiers:[ ],
 
-                    if( checkValue(orgarray[i].name) ){organization.name = orgarray[i].name;}
+                    if( checkValue(array[i].name) ){organization.name = array[i].name;}
 
                     organization.address = {};
-                    if( checkValue(orgarray[i].address_streetaddress) ){organization.address.streetAddress = orgarray[i].address_streetaddress;}
-                    if( checkValue(orgarray[i].address_locality) ){organization.address.locality = orgarray[i].address_locality;}
-                    if( checkValue(orgarray[i].address_region) ){organization.address.region = orgarray[i].address_region;}
-                    if( checkValue(orgarray[i].address_postalcode) ){organization.address.postalCode = orgarray[i].address_postalcode;}
-                    if( checkValue(orgarray[i].address_countryname) ){organization.address.countryName = orgarray[i].address_countryname;}
+                    if( checkValue(array[i].address_streetaddress) ){organization.address.streetAddress = array[i].address_streetaddress;}
+                    if( checkValue(array[i].address_locality) ){organization.address.locality = array[i].address_locality;}
+                    if( checkValue(array[i].address_region) ){organization.address.region = array[i].address_region;}
+                    if( checkValue(array[i].address_postalcode) ){organization.address.postalCode = array[i].address_postalcode;}
+                    if( checkValue(array[i].address_countryname) ){organization.address.countryName = array[i].address_countryname;}
 
                     organization.contactPoint = {};
-                    if( checkValue(orgarray[i].contactpoint_name) ){organization.contactPoint.name = orgarray[i].contactpoint_name;}
-                    if( checkValue(orgarray[i].contactpoint_email) ){organization.contactPoint.email = orgarray[i].contactpoint_email;}
-                    if( checkValue(orgarray[i].contactpoint_telephone) ){organization.contactPoint.telephone = orgarray[i].contactpoint_telephone;}
-                    if( checkValue(orgarray[i].contactpoint_faxnumber) ){organization.contactPoint.faxNumber = orgarray[i].contactpoint_faxnumber;}
-                    if( checkValue(orgarray[i].contactpoint_url) ){organization.contactPoint.url = orgarray[i].contactpoint_url;}
+                    if( checkValue(array[i].contactpoint_name) ){organization.contactPoint.name = array[i].contactpoint_name;}
+                    if( checkValue(array[i].contactpoint_email) ){organization.contactPoint.email = array[i].contactpoint_email;}
+                    if( checkValue(array[i].contactpoint_telephone) ){organization.contactPoint.telephone = array[i].contactpoint_telephone;}
+                    if( checkValue(array[i].contactpoint_faxnumber) ){organization.contactPoint.faxNumber = array[i].contactpoint_faxnumber;}
+                    if( checkValue(array[i].contactpoint_url) ){organization.contactPoint.url = array[i].contactpoint_url;}
 
                     organizations.push(organization);
 
@@ -876,20 +876,22 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
             }
 
 
-            function getDocuments(docarray){
+            function getDocuments(array){
                 var documents =[];
-                for (var i=0; i < docarray.length; i++ ){
-                    documents.push({
-                        id: docarray[i].id,
-                        documentType: docarray[i].document_type,
-                        title: docarray[i].title ,
-                        description: docarray[i].description,
-                        url: docarray[i].url,
-                        datePublished: docarray[i].date_published,
-                        dateModified: docarray[i].date_modified,
-                        format: docarray[i].format,
-                        language: docarray[i].language
-                    });
+                for (var i=0; i < array.length; i++ ){
+                    var document = { };
+
+                    if(checkValue(array[i].id)){document.id = array[i].id;}
+                    if(checkValue(array[i].document_type)){document.documentType = array[i].document_type;}
+                    if(checkValue(array[i].title)){document.title = array[i].title;}
+                    if(checkValue(array[i].description)){document.description = array[i].description;}
+                    if(checkValue(array[i].url)){document.url = array[i].url;}
+                    if(checkValue(array[i].date_published)){document.datePublished = array[i].date_published;}
+                    if(checkValue(array[i].date_modified)){document.dateModified = array[i].date_modified;}
+                    if(checkValue(array[i].format)){document.format = array[i].format;}
+                    if(checkValue(array[i].language)){document.language = array[i].language;}
+
+                    documents.push(document);
                 }
                 return documents;
             }
@@ -897,25 +899,26 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
             function getItems(arr){
                 var items =[];
                 for (var i=0; i < arr.length;i++){
-                    items.push({
-                        id: arr[i].id,
-                        description: arr[i].description,
-                        //additionalClasifications: [ ],
-                        classification:{
-                            scheme: arr[i].classification_scheme,
-                            id: arr[i].classification_id,
-                            description: arr[i]. classification_description,
-                            uri: arr[i].classification_uri
-                        },
-                        quantity: arr[i].quantity,
-                        unit :{
-                            name: arr[i].unit_name,
-                            value: {
-                                amount: Number(arr[i].unit_value_amount),
-                                currency: arr[i].unit_value_currency
-                            }
-                        }
-                    });
+                    var item = { };
+                    if(checkValue(arr[i].id)){item.id = arr[i].id;}
+                    if(checkValue(arr[i].description)){item.description = arr[i].description;}
+                    //additionalClasifications: [ ],
+
+                    item.classification = { };
+                    if(checkValue(arr[i].classification_scheme)){item.classification.scheme = arr[i].classification_scheme;}
+                    if(checkValue(arr[i].classification_id)){item.classification.id = arr[i].classification_id;}
+                    if(checkValue(arr[i].classification_description)){item.classification.description = arr[i]. classification_description;}
+                    if(checkValue(arr[i].classification_uri)){item.classification.uri = arr[i].classification_uri;}
+
+                    if(checkValue(arr[i].quantity)){item.quantity = arr[i].quantity;}
+
+                    item.unit = { };
+                    if(checkValue(arr[i].unit_name)){item.unit.name = arr[i].unit_name;}
+                    item.unit.value = { };
+                    if(checkValue(arr[i].unit_value_amount)){item.unit.value.amount = Number(arr[i].unit_value_amount);}
+                    if(checkValue(arr[i].unit_value_currency)){item.unit.value.currency = arr[i].unit_value_currency;}
+
+                    items.push(item);
                 }
                 return items;
             }
@@ -923,13 +926,15 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
             function getMilestones(arr) {
                 var milestones =[];
                 for (var i=0; i < arr.length;i++){
-                    milestones.push({
-                        title: arr[i].title,
-                        description: arr[i].description,
-                        dueDate: arr[i].duedate,
-                        dateModified: arr[i].date_modified,
-                        status: arr[i].status
-                    });
+                    var milestone = { };
+
+                    if(checkValue(arr[i].title)){milestone.title = arr[i].title;}
+                    if(checkValue(arr[i].description)){milestone.description = arr[i].description;}
+                    if(checkValue(arr[i].duedate)){milestone.dueDate = arr[i].duedate;}
+                    if(checkValue(arr[i].date_modified)){milestone.dateModified = arr[i].date_modified;}
+                    if(checkValue(arr[i].status)){milestone.status = arr[i].status;}
+
+                    milestones.push(milestone);
                 }
                 return milestones;
             }
@@ -938,29 +943,32 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
                 var transactions = [];
 
                 for (var i =0; i< arr.length;i++){
-                    transactions.push({
-                        source: arr[i].source,
-                        date: arr[i].date,
-                        value : {
-                            amount: Number(arr[i].value_amount),
-                            currency: arr[i].currency
-                        },
-                        providerOrganization:{
-                            scheme: arr[i].providerorganization_scheme,
-                            id: arr[i].providerorganization_id,
-                            legalName: arr[i].providerorganization_legalname,
-                            uri: arr[i].providerorganization_uri
-                        },
-                        receiverOrganization:{
-                            scheme: arr[i].receiverorganization_scheme,
-                            id: arr[i].receiverorganization_id,
-                            legalName: arr[i].receiverorganization_legalname,
-                            uri: arr[i].receiverorganization_uri
-                        },
-                        uri: arr[i].uri
-                    });
-                }
+                    var transaction = { };
 
+                    if(checkValue(arr[i].source)){transaction.source = arr[i].source;}
+                    if(checkValue(arr[i].date)){transaction.date = arr[i].date;}
+
+                    transaction.value = { };
+                    if(checkValue(arr[i].value_amount)){transaction.value.amount = Number(arr[i].value_amount);}
+                    if(checkValue(arr[i].currency)){transaction.value.currency = arr[i].currency;}
+
+                    transaction.providerOrganization = { };
+                    if(checkValue(arr[i].providerorganization_scheme)){transaction.providerOrganization.scheme = arr[i].providerorganization_scheme;}
+                    if(checkValue(arr[i].providerorganization_id)){transaction.providerOrganization.id = arr[i].providerorganization_id;}
+                    if(checkValue(arr[i].providerorganization_legalname)){transaction.providerOrganization.legalName = arr[i].providerorganization_legalname;}
+                    if(checkValue(arr[i].providerorganization_uri)){transaction.providerOrganization.uri = arr[i].providerorganization_uri;}
+
+
+                    transaction.receiverOrganization = { };
+                    if(checkValue(arr[i].receiverorganization_scheme)){transaction.receiverOrganization.scheme = arr[i].receiverorganization_scheme;}
+                    if(checkValue(arr[i].receiverorganization_id)){transaction.receiverOrganization.id = arr[i].receiverorganization_id;}
+                    if(checkValue(arr[i].receiverorganization_legalname)){transaction.receiverOrganization.legalName = arr[i].receiverorganization_legalname;}
+                    if(checkValue(arr[i].receiverorganization_uri)){transaction.receiverOrganization.uri = arr[i].receiverorganization_uri;}
+
+                    if(checkValue(arr[i].uri)){transaction.uri = arr[i].uri;}
+
+                    transactions.push(transaction);
+                }
                 return transactions;
             }
 
@@ -996,17 +1004,29 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
             if (checkValue(data[0].budget.budget_amount)){release.planning.budget.amount.amount = Number(data[0].budget.budget_amount);}
             if (checkValue(data[0].budget.budget_currency)){release.planning.budget.amount.currency = data[0].budget.budget_currency;}
 
-
             if (checkValue(data[0].budget.budget_project)){release.planning.budget.project = data[0].budget.budget_project;}
             if (checkValue(data[0].budget.budget_projectid)){release.planning.budget.projectID = data[0].budget.budget_projectid;}
             if (checkValue(data[0].budget.budget_uri)){release.planning.budget.uri = data[0].budget.budget_uri;}
 
             if (checkValue(data[0].planning.rationale)){release.planning.rationale = data[0].planning.rationale;}
 
-
             //planning documents
             if (data[4].length > 0){
                 release.planning.documents = getDocuments(data[4])
+            }
+
+            //Limpia la etapa de planeación
+            // planning.budget.amount == {} -> eliminar
+            if (JSON.stringify(release.planning.budget.amount) === JSON.stringify({})){
+                delete release.planning.budget['amount'];
+            }
+            // planning.budget == {} -> eliminar
+            if (JSON.stringify(release.planning.budget) === JSON.stringify({})){
+                delete release.planning['budget'];
+            }
+            // planning == {} -> eliminar
+            if (JSON.stringify(release.planning) === JSON.stringify({})){
+                delete release['planning'];
             }
 
             //TENDER
@@ -1078,6 +1098,32 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
 
             if (checkValue(data[0].tender.amendment_rationale)){release.tender.amendment.rationale = data[0].tender.amendment_rationale;}
 
+            //limpia la etapa de licitación
+            if (JSON.stringify(release.tender.minValue) === JSON.stringify({})){
+                delete release.tender['minValue'];
+            }
+
+            if (JSON.stringify(release.tender.value) === JSON.stringify({})){
+                delete release.tender['value'];
+            }
+            if (JSON.stringify(release.tender.tenderPeriod) === JSON.stringify({})){
+                delete release.tender['tenderPeriod'];
+            }
+            if (JSON.stringify(release.tender.enquiryPeriod) === JSON.stringify({})){
+                delete release.tender['enquiryPeriod'];
+            }
+            if (JSON.stringify(release.tender.awardPeriod) === JSON.stringify({})){
+                delete release.tender['awardPeriod'];
+            }
+
+            if (JSON.stringify(release.tender.amendment) === JSON.stringify({})){
+                delete release.tender['amendment'];
+            }
+
+            if (JSON.stringify(release.tender) === JSON.stringify({})){
+                delete release['tender'];
+            }
+
             //BUYER
             release.buyer = getOrganizations( [ data[0].buyer ]);
 
@@ -1120,7 +1166,20 @@ router.get('/publish/:type/:localid/:outputname', function (req,res) {
 
             if(checkValue(data[0].award.amendment_rationale)){award.amendment.rationale = data[0].award.amendment_rationale;}
 
-            release. awards = [ award ];
+            //limpia la adjudicación
+            if (JSON.stringify(award.value) === JSON.stringify({})){
+                delete award['value'];
+            }
+            if (JSON.stringify(award.contractPeriod) === JSON.stringify({})){
+                delete award['contractPeriod'];
+            }
+            if (JSON.stringify(award.amendment) === JSON.stringify({})){
+                delete award['amendment'];
+            }
+
+            if (JSON.stringify(award) !== JSON.stringify({})){
+                release. awards = [ award ];
+            }
 
             //CONTRACTS
             var contract = { };//pueden ser varios
