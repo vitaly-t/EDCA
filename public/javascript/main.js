a2a9062550ae4633ed819ff430fa38e74ad1f2e9
Mario Torres <mario291187@gmail.com>
2016-09-27 14:25:34 -0500

    Edit milestones

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -301,24 +301,6 @@ $('#myModalEditDocuments').on('show.bs.modal', function (event) {
     });
 });
 
-$('#myModalEditMilestones').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var modal = $(this);
-    modal.find('.modal-body div').load( '/milestone-list/' ,{ ocid: button.data('ocid'), table : button.data('table') });
-    //button events
-    var div = modal.find('.modal-body div');
-    div.off('click','.btn');
-    div.on('click', '.btn', function (event) {
-        var b = $(this);
-        $.post('/delete', { id : b.data('id'), table: b.data('table') }).done(function(data){
-            alert(data.msg);
-            if ( data.status == 0 ){
-                b.parent().parent().remove();
-            }
-        });
-    });
-});
-
 $('#genericModal').on('show.bs.modal', function (event) {
    var button = $(event.relatedTarget);
     var modal = $(this);
@@ -456,6 +438,19 @@ $('#genericModal').on('show.bs.modal', function (event) {
         case "edit_milestones":
             modal.find('.modal-title').text('Editar hitos');
             modal.find('#modal_content').html("");
+            modal.find('#modal_content').load( '/milestone-list/' ,{ ocid: button.data('contractingprocess_id'), table : button.data('table') },function () {
+                //button events
+                var div = modal.find('#modal_content');
+                div.find('.btn').click(function () {
+                    var b = $(this);
+                    $.post('/delete', { id : b.data('id'), table: b.data('table') }).done(function(data){
+                        alert(data.msg);
+                        if ( data.status == 0 ){
+                            b.parent().parent().remove();
+                        }
+                    });
+                });
+            });
             break;
         case "edit_organizations":
             modal.find('.modal-title').text('Nueva organización');
