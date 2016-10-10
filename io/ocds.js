a6fd681f66f0bae283ff1368962eee3bc4ed2508
Mario Torres <mario291187@gmail.com>
2016-10-10 17:34:59 -0500

    ...

--- io/ocds.js
+++ io/ocds.js
@@ -72,7 +72,7 @@ module.exports = {
 
                 function deleteNullProperties(test, recursive) {
                     for (var i in test) {
-                        if (test[i] === null || JSON.stringify(test[i]) === JSON.stringify({}) ) {
+                        if (test[i] === null || JSON.stringify(test[i]) === JSON.stringify({}) || test[i]=='' ) {
                             delete test[i];
                         } else if (recursive && typeof test[i] === 'object') {
                             deleteNullProperties(test[i], recursive);
