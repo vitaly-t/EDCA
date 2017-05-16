9cf63ede8b820f1a669e1bdb83cf1b43aa6ee717
Mario Torres <mario291187@gmail.com>
2017-05-16 13:21:38 -0500

    adding dot env file support

--- db_conf.js
+++ db_conf.js
@@ -3,34 +3,17 @@ var options = {};
 
 pgp = require('pg-promise')(options);
 
-var edca_db;
+const config = {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
+    database: process.env.POSTGRES_NAME ||'edca',
+    user: process.env.POSTGRES_USER || 'tester',
+    password: process.env.POSTGRES_PASSWORD || 'test'
+};
 
-// Linked postgresql docker container
-if ( typeof process.env.POSTGRES_PORT_5432_TCP_ADDR != "undefined" ) {
-    process.env.EDCA_DB = 'postgres://';
-    process.env.EDCA_DB += process.env.POSTGRES_USER || 'postgres';
-    process.env.EDCA_DB += ':';
-    process.env.EDCA_DB += process.env.POSTGRES_ENV_POSTGRES_PASSWORD || '';
-    process.env.EDCA_DB += '@';
-    process.env.EDCA_DB += process.env.POSTGRES_PORT_5432_TCP_ADDR;
-    process.env.EDCA_DB += '/';
-    process.env.EDCA_DB += process.env.POSTGRES_DB || 'postgres';
-}
+var edca_db = pgp(config);
 
-if ( typeof process.env.EDCA_DB != "undefined" ){
-    console.log("EDCA_DB: ", process.env.EDCA_DB);
-    edca_db = pgp( process.env.EDCA_DB );
-} else {
-    console.log("Warning: EDCA_DB env variable is not set\n " +
-        " defaulting to -> postgres://tester:test@localhost/edca");
-    edca_db = pgp({
-        host: 'localhost',
-        //port: 5433,
-        database: 'edca',
-        user: 'tester',
-        password: 'test'
-    });
-}
+console.log('DB Config -> ', JSON.stringify(config));
 
 
 module.exports = {
